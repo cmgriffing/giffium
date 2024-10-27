33dc1abf23e04d0cd27888cda3b9f0330a56483e
Kushal Gaywala <kushalgaywala@gmail.com>
2024-10-27 17:40:53 +0100

    feat: Added GIF in 404 router

--- playgrounds/app/src/routes/[...404].tsx
+++ playgrounds/app/src/routes/[...404].tsx
@@ -3,10 +3,10 @@ import { A } from "@solidjs/router";
 export default function NotFound() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">404: Not Found</h1>
+      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">404: Page Not Found</h1>
       <p class="mt-8">
-        Didn't find what you were looking? 
-        <br />
+        Didn't find what you were looking?
+        <img src="../../public/404-gif-468x328.gif" alt="404: Page Not Found (Confused Tarvolta)" class="mx-auto my-4" />
         You might want to visit the {" "}
         <A href="/about" class="text-sky-600 hover:underline">
           About Page
